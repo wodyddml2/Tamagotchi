4f4aa7efd4e73aceb613dd453c3bf3b090b35be9
wodyddml2 <wodyddml2@naver.com>
2022-07-24 00:44:46 +0900

    설정화면 초기진입

--- Tamagotchi/Setting/SettingTableViewCell.swift
+++ Tamagotchi/Setting/SettingTableViewCell.swift
@@ -2,5 +2,8 @@
 import UIKit
 
 class SettingTableViewCell: UITableViewCell {
-
+    @IBOutlet weak var settingImage: UIImageView!
+    @IBOutlet weak var settingTitle: UILabel!
+    @IBOutlet weak var user: UILabel!
+    
 }
--- Tamagotchi/Setting/SettingTableViewController.swift
+++ Tamagotchi/Setting/SettingTableViewController.swift
@@ -6,10 +6,30 @@ class SettingTableViewController: UITableViewController {
     static let settingIdentifier = "SettingTableViewController"
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = UIColor(red: 245/255, green: 252/255, blue: 252/255, alpha: 1)
+        
+        navigationItem.title = "설정"
+        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor(named: "NameColor") ?? .black]
        
     }
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "SettingTableViewCell", for: indexPath) as? SettingTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.backgroundColor = UIColor(red: 245/255, green: 252/255, blue: 252/255, alpha: 1)
+        cell.accessoryType = .disclosureIndicator
+        if indexPath.row == 0 {
+            cell.user.text = "\(ViewController.userName)"
+        } else {
+            cell.user.text = ""
+        }
+        return cell
+    }
    
 
 }
